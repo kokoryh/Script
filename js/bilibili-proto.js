5218ad9c4ea2a89327e06c5d0481216942e13fb6
kokoryh <kokoryh@qq.com>
2022-11-23 22:56:50 +0800

    Update bilibili-proto.js

--- js/bilibili-proto.js
+++ js/bilibili-proto.js
@@ -40,11 +40,10 @@ if(url.includes("Dynamic/DynAll")){
 
     if(!dynAllReplyObj.upList){
         // console.log('upList为空');
-    } else if(dynAllReplyObj.upList.list[0].userItemType !== 3) {
-        // 非normal情况，如直播(type = 1)
+    } else if(dynAllReplyObj.upList.showLiveNum !== 0) {
         // console.log('有直播,不去除upList');
-        // dynAllReplyObj.upList.title = '正在直播';
-        // dynAllReplyObj.upList.moreLabel.title = '我的关注';
+        dynAllReplyObj.upList.title = '正在直播';
+        dynAllReplyObj.upList.moreLabel.title = '我的关注';
         // dynAllReplyObj.upList.list = dynAllReplyObj.upList.list.slice(0, dynAllReplyObj.upList.showLiveNum);
         // dynAllReplyObj.upList.list[dynAllReplyObj.upList.showLiveNum - 1].separator = undefined;
     } else {
