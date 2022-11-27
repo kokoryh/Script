0ecd0b775319e10ea868ab4511828cf87ad595ec
kokoryh <kokoryh@qq.com>
2022-11-28 07:41:19 +0800

    更新12306

---
+++ js/12306.js
@@ -0,0 +1,12 @@
+var obj = JSON.parse($response.body);
+if (obj.materialsList) {
+    if (obj.materialsList.length === 1) {
+        obj.materialsList[0].filePath = "";
+        // obj.advertParam.skipTime = 1;
+        // obj.advertParam.skipTimeAgain = 5;
+        obj.advertParam.showSkipBtn = 0;
+    } else if (obj.materialsList.length > 1) {
+        obj.materialsList = [];
+    }
+}
+$done({body: JSON.stringify(obj)});
