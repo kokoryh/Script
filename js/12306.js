2cc2e1ec475a70ee4be163166e7e663f365b488f
kokoryh <kokoryh@qq.com>
2022-11-28 10:49:48 +0800

    Update 12306.js

--- js/12306.js
+++ js/12306.js
@@ -3,8 +3,6 @@ if (obj.materialsList) {
     if (obj.materialsList.length === 1) {
         obj.materialsList[0].filePath = "";
         obj.advertParam.skipTime = 50;
-        obj.advertParam.showSkipBtn = -1;
-        obj.advertParam.skipTimeAgain = 60;
     } else if (obj.materialsList.length > 1) {
         obj.materialsList = [{}];
     }
