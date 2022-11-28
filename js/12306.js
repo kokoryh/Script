7b5f4f0276bcab8e9a081212d34a0ab895846170
kokoryh <kokoryh@qq.com>
2022-11-28 08:15:26 +0800

    Update 12306.js

--- js/12306.js
+++ js/12306.js
@@ -2,11 +2,11 @@ var obj = JSON.parse($response.body);
 if (obj.materialsList) {
     if (obj.materialsList.length === 1) {
         obj.materialsList[0].filePath = "";
-        obj.advertParam.skipTime = 1;
+        obj.advertParam.skipTime = 100;
         obj.advertParam.skipTimeAgain = 5;
         obj.advertParam.showSkipBtn = 0;
-    } else if (obj.materialsList.length > 1) {
-        obj.materialsList = [];
+    } else if (obj.materialsList.length > 1 && obj.advertParam.fixedscreen !== -1) {
+        obj.materialsList = [{}];
     }
 }
 $done({body: JSON.stringify(obj)});
