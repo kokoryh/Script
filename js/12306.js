5c409bd41e2356beb317903cf5aee66fca39bf2d
kokoryh <kokoryh@qq.com>
2022-11-28 08:44:27 +0800

    修正

--- js/12306.js
+++ js/12306.js
@@ -3,8 +3,6 @@ if (obj.materialsList) {
     if (obj.materialsList.length === 1) {
         obj.materialsList[0].filePath = "";
         obj.advertParam.skipTime = 100;
-        obj.advertParam.skipTimeAgain = 60;
-        obj.advertParam.showSkipBtn = 0;
     } else if (obj.materialsList.length > 1) {
         obj.materialsList = [{}];
     }
