fa03fc0ec4be229ddf0af253ce8f3819f0d7803f
kokoryh <kokoryh@qq.com>
2022-12-02 11:01:42 +0800

    Update bili-manga.js

--- js/bili-manga.js
+++ js/bili-manga.js
@@ -1,5 +1,6 @@
 var obj = JSON.parse($response.body);
 if (obj.data && obj.data.operate) {
     obj.data.operate = null;
+    obj.data.teenager_frequency = 0;
 }
 $done({body: JSON.stringify(obj)});
