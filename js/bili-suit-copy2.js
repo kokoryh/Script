57b4fc232b685facc25cf76ad0e6d32648506dc9
kokoryh <kokoryh@qq.com>
2022-12-03 07:37:37 +0800

    bili-suit-copy ver1.3

---
+++ js/bili-suit-copy2.js
@@ -0,0 +1,17 @@
+var body = $response.body;
+if (body && body.length < 45) {
+    var obj = JSON.parse(body);
+    var user_equip = $prefs.valueForKey("bili_user_equip");
+    var load_equip = $prefs.valueForKey("bili_load_equip");
+    if(user_equip) {
+        user_equip = JSON.parse(user_equip);
+        obj.data["user_equip"] = user_equip;
+    }
+    if(load_equip) {
+        load_equip = JSON.parse(load_equip);
+        obj.data["load_equip"] = load_equip;
+    }
+    $done({body: JSON.stringify(obj)});
+}else {
+    $done();
+}
