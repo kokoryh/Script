4e484c2e0eed23afef7d393a513a5eea0f8ecd77
kokoryh <kokoryh@qq.com>
2022-12-03 21:43:23 +0800

    Update bili-suit-detail.js

--- js/bili-suit-detail.js
+++ js/bili-suit-detail.js
@@ -51,12 +51,12 @@ if (body) {
     var data = JSON.parse(body).data;
     var skin = data.suit_items.skin[skin_num -1];
     // 针对忘记把bili_skin_num参数改回的情况，自动递减直到有skin数据
-    for(let i = 1; i < skin_num; i++) {
+    for(let i = 0; i < skin_num; i++) {
         if(!skin){
-            skin = data.suit_items.skin[skin_num - 1 - i];
+            skin = data.suit_items.skin[skin_num - 2 - i];
         } else {
-            if(i !== 1) {
-                skin_num = skin_num -i + 1;
+            if(i !== 0) {
+                skin_num = skin_num - i;
                 $.setdata("1", "bili_skin_num");
                 skin_num_notice = "\n你设置的skin_num值过大，已重置为1！";
             }
@@ -102,7 +102,7 @@ if (body) {
         success2 = $.setdata(JSON.stringify(load_equip), "bili_load_equip");
     }
     if (success1) {
-        $.msg("获取装扮信息成功 🎉️", "", `当前第${skin_num}套装扮：` + skin_num_notice);
+        $.msg("获取装扮信息成功 🎉️", "", `当前第${skin_num}套装扮：` + user_equip.name + skin_num_notice);
     } else {
         $.msg("获取user_equip失败 ‼️", "", "");
     }
