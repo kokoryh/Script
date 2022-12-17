b30e6254bd60aaf6378fc25967dff7921f00e218
kokoryh <kokoryh@qq.com>
2022-12-17 20:22:20 +0800

    bili-suit-diy ver1.7.3 增加不对粉色B站生效选项

--- js/bili-suit-diy.js
+++ js/bili-suit-diy.js
@@ -1,6 +1,6 @@
 /*
 B站装扮diy
-版本：1.7.2
+版本：1.7.3
 脚本兼容: Quantumult X
 作者：@kokoryh
 
@@ -14,7 +14,7 @@ B站装扮diy
 4、如果装扮有多套主题，可在boxjs中填入『使用第几套主题』，加载动画同理
 5、如果想追加其他装扮，可在boxjs中将『装扮追加』开关打开
 6、粉色B站和白色B站均可使用本脚本
-7、如果只想让白色B站使用本脚本，而粉色B站不使用，请使用bili-suit-diy2.js
+7、如果只想让白色B站使用本脚本，而粉色B站不使用，可在boxjs中将『对粉色B站不生效』开关打开
 8、diy请自行下载需要的装扮，将素材拼合为规范的zip包上传，然后自行编写规范的配置填入boxjs。仅适合有一定编程基础的人，小白请放弃此功能
    配置格式和各配置项含义请『自行体会』，作为挡住小白的门槛
 9、空间头图和头像框均可以替换，但由于我用不到，因此不会实现此功能。有需要请自行实现
@@ -39,34 +39,39 @@ hostname = app.bilibili.com
 ----------------
 */
 var obj = JSON.parse($response.body);
-var user_equip = $prefs.valueForKey("bili_user_equip");
-var load_equip = $prefs.valueForKey("bili_load_equip");
-if(user_equip) {
-    user_equip = JSON.parse(user_equip);
-    if (user_equip.length === 1) obj.data["user_equip"] = user_equip[0];
-    else {
-        var skin_num = parseInt($prefs.valueForKey("bili_skin_num") || 1);
-        if (skin_num > user_equip.length) {
-            obj.data["user_equip"] = user_equip[0];
-            $prefs.setValueForKey("1", "bili_skin_num");
-            $notify("bili_skin_num参数设置过大", "", "转为使用第1套主题，bili_skin_num已重置为1");
-        } else {
-            obj.data["user_equip"] = user_equip[skin_num - 1];
+var skip_pink = $prefs.valueForKey("bili_skip_pink") === "true";
+if (skip_pink && obj.data.skin_colors) {
+    $done();
+} else {
+    var user_equip = $prefs.valueForKey("bili_user_equip");
+    var load_equip = $prefs.valueForKey("bili_load_equip");
+    if (user_equip) {
+        user_equip = JSON.parse(user_equip);
+        if (user_equip.length === 1) obj.data["user_equip"] = user_equip[0];
+        else {
+            var skin_num = parseInt($prefs.valueForKey("bili_skin_num") || 1);
+            if (skin_num > user_equip.length) {
+                obj.data["user_equip"] = user_equip[0];
+                $prefs.setValueForKey("1", "bili_skin_num");
+                $notify("bili_skin_num参数设置过大", "", "转为使用第1套主题，bili_skin_num已重置为1");
+            } else {
+                obj.data["user_equip"] = user_equip[skin_num - 1];
+            }
         }
     }
-}
-if(load_equip) {
-    load_equip = JSON.parse(load_equip);
-    if (load_equip.length === 1) obj.data["load_equip"] = load_equip[0];
-    else {
-        var load_num = parseInt($prefs.valueForKey("bili_load_num") || 1);
-        if (load_num > load_equip.length) {
-            obj.data["load_equip"] = load_equip[0];
-            $prefs.setValueForKey("1", "bili_load_num");
-            $notify("bili_load_num参数设置过大", "", "转为使用第1个加载动画，bili_load_num已重置为1");
-        } else {
-            obj.data["load_equip"] = load_equip[load_num - 1];
+    if (load_equip) {
+        load_equip = JSON.parse(load_equip);
+        if (load_equip.length === 1) obj.data["load_equip"] = load_equip[0];
+        else {
+            var load_num = parseInt($prefs.valueForKey("bili_load_num") || 1);
+            if (load_num > load_equip.length) {
+                obj.data["load_equip"] = load_equip[0];
+                $prefs.setValueForKey("1", "bili_load_num");
+                $notify("bili_load_num参数设置过大", "", "转为使用第1个加载动画，bili_load_num已重置为1");
+            } else {
+                obj.data["load_equip"] = load_equip[load_num - 1];
+            }
         }
     }
+    $done({body: JSON.stringify(obj)});
 }
-$done({body: JSON.stringify(obj)});
