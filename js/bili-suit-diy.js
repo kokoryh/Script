325559df3bf4afd63b99063a88b3d5d6fc80c008
kokoryh <kokoryh@qq.com>
2022-12-18 10:11:37 +0800

    bili-suit-diy 1.8.0

--- js/bili-suit-diy.js
+++ js/bili-suit-diy.js
@@ -1,6 +1,6 @@
 /*
 B站装扮diy
-版本：1.7.3
+版本：1.8.0
 脚本兼容: Quantumult X
 作者：@kokoryh
 
@@ -9,14 +9,13 @@ B站装扮diy
 
 说明：
 1、打开粉色B站-我的-个性装扮，选择喜欢的装扮进入装扮详情页，如果通知获取装扮信息成功，则可以使用本脚本
-2、更换新的装扮需要退后台重新打开app，重复两次
+2、更换新的装扮需要退后台重新打开app，重复2次
 3、如果不想提取加载动画，可在boxjs中将『不提取加载动画』开关打开
-4、如果装扮有多套主题，可在boxjs中填入『使用第几套主题』，加载动画同理
-5、如果想追加其他装扮，可在boxjs中将『装扮追加』开关打开
-6、粉色B站和白色B站均可使用本脚本
-7、如果只想让白色B站使用本脚本，而粉色B站不使用，可在boxjs中将『对粉色B站不生效』开关打开
-8、diy请自行下载需要的装扮，将素材拼合为规范的zip包上传，然后自行编写规范的配置填入boxjs。仅适合有一定编程基础的人，小白请放弃此功能
-   配置格式和各配置项含义请『自行体会』，作为挡住小白的门槛
+4、如果想保留原有装扮，可在boxjs中将『装扮追加』开关打开
+5、如果有多套主题，可在boxjs中填入『使用第几套主题』，加载动画同理。填写前请仔细阅读该选项下的简介
+6、装扮的定时切换请按照定时脚本的要求正确配置
+7、粉色B站和白色B站均可使用本脚本
+8、diy请自行下载需要的装扮，将素材拼合为规范的zip包上传，然后自行编写规范的配置填入boxjs。仅适合有一定编程基础的人，配置格式和各配置项含义请『自行体会』，作为挡住小白的门槛
 9、空间头图和头像框均可以替换，但由于我用不到，因此不会实现此功能。有需要请自行实现
 10、引用请自行去掉前面的#号，用解析器解析的都给我滚
 
@@ -27,7 +26,6 @@ B站装扮diy
 
 [mitm]
 hostname = api.bilibili.com
-
 ----------------
 日常使用
 [rewrite_local]
@@ -35,15 +33,16 @@ hostname = api.bilibili.com
 
 [mitm]
 hostname = app.bilibili.com
-
+----------------
+定时切换脚本(此处cron仅供参考，请自行设置切换的时间)
+[task_local]
+0 8,20 * * * https://raw.githubusercontent.com/kokoryh/Script/master/js/bili-suit-change.js, tag=装扮定时切换, img-url=https://raw.githubusercontent.com/NobyDa/mini/master/Color/bilibili.png, enabled=true
 ----------------
 */
-let obj = JSON.parse($response.body);
-let type = getType(obj);
-let skip_pink = $prefs.valueForKey("bili_skip_pink") === "true";
-if (skip_pink && !type) {
-    $done();
-} else {
+let body = $response.body;
+if (body) {
+    let obj = JSON.parse(body);
+    let type = getType(obj);
     let user_equip = $prefs.valueForKey("bili_user_equip");
     let load_equip = $prefs.valueForKey("bili_load_equip");
     let skin_num = $prefs.valueForKey("bili_skin_num");
@@ -51,6 +50,8 @@ if (skip_pink && !type) {
     setEquip(user_equip, skin_num, type, "user_equip");
     setEquip(load_equip, load_num, type, "load_equip");
     $done({body: JSON.stringify(obj)});
+} else {
+    $done();
 }
 
 function setEquip(equip, equip_num, type, param) {
@@ -62,7 +63,7 @@ function setEquip(equip, equip_num, type, param) {
             obj.data[param] = equip_list[num - 1];
         } else {
             obj.data[param] = equip_list[0];
-            $notify(`参数设置有误`, "", "请检查BoxJs设置");
+            $notify("B站装扮DIY", "", "参数设置过大，请检查BoxJs设置");
         }
     }
 }
@@ -70,10 +71,13 @@ function setEquip(equip, equip_num, type, param) {
 function getNum(num, type) {
     if (!num) return 1;
     else if (parseInt(num) >= 0) return num;
-    else return num.split(/;|；/)[type + 1].split(":")[0];
+    else if (/([;；]\d+(:\d+)?){2}/.test(num)) return num.split(/;|；/)[type + 1].split(":")[0];
+    else {
+        $notify("B站装扮DIY", "", "参数格式非法，请检查BoxJs设置");
+        return 1;
+    }
 }
 
-// 判断是粉色B站还是白色B站, 0为粉色B站, 1为白色B站
 function getType(obj) {
     if (obj.data.skin_colors) return 0;
     return 1;
