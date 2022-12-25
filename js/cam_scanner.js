a2aa58d4e70fd967fff74066fae42079e9c4d0a4
kokoryh <kokoryh@qq.com>
2022-12-25 18:50:05 +0800

    update

---
+++ js/cam_scanner.js
@@ -0,0 +1,1 @@
+let obj={data:{psnl_vip_property:{expiry:"3287462400"}}};$done({body:JSON.stringify(obj)});
--- js/dev/12306.js
+++ js/dev/12306.js
@@ -3,6 +3,8 @@ if (obj.materialsList) {
     if (obj.materialsList.length === 1) {
         obj.materialsList[0].filePath = "";
         obj.advertParam.skipTime = 10;
+        obj.advertParam.skipTimeAgain = 5;
+        obj.advertParam.showSkipBtn = -1;
     } else if (obj.materialsList.length > 1) {
         obj.materialsList = [{}];
     }
