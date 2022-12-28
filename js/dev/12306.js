4d7804a22b6a702f997c66b3333e0632cfd072d5
kokoryh <kokoryh@qq.com>
2022-12-28 18:20:57 +0800

    Update 12306.js

--- js/dev/12306.js
+++ js/dev/12306.js
@@ -2,9 +2,8 @@ var obj = JSON.parse($response.body);
 if (obj.materialsList) {
     if (obj.materialsList.length === 1) {
         console.log(obj.materialsList[0].title);
-        obj.materialsList[0].filePath = "https://raw.githubusercontent.com/kokoryh/Script/master/data/blank.jpg";
+        // obj.materialsList[0].filePath = "https://raw.githubusercontent.com/kokoryh/Script/master/data/blank.jpg";
         obj.advertParam.skipTime = 1;
-        // obj.advertParam.lg = 1;
     } else if (obj.materialsList.length > 1) {
         obj.materialsList = [{}];
     }
