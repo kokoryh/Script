d8d938efe8bb0f5b367714ea144d8f39400f0eff
kokoryh <kokoryh@qq.com>
2022-12-28 18:33:41 +0800

    更新12306去广告

--- js/dev/12306.js
+++ js/dev/12306.js
@@ -1,7 +1,6 @@
 var obj = JSON.parse($response.body);
 if (obj.materialsList) {
     if (obj.materialsList.length === 1) {
-        console.log(obj.materialsList[0].title);
         obj.materialsList[0].filePath = undefined;
         obj.advertParam.skipTime = 1;
     } else if (obj.materialsList.length > 1) {
