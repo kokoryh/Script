340287c909cf5d1fa3798354b4c0dfc52205dc28
kokoryh <kokoryh@qq.com>
2022-12-29 14:31:59 +0800

    更新去除12306开屏广告的方式

--- js/dev/12306.js
+++ js/dev/12306.js
@@ -1,10 +1,10 @@
 if (typeof $response !== 'undefined') {
-    removeAds2();
+    removeAds();
 } else {
     removeValue();
 }
 
-function removeAds2() {
+function removeAds() {
     var obj = JSON.parse($response.body);
     if (obj.materialsList) {
         if (obj.materialsList.length === 1) {
@@ -34,37 +34,37 @@ function setData(obj) {
     obj.advertParam.chacheTime = 86400 * 365 * 10;
 }
 
-function removeAds() {
-    var obj = JSON.parse($response.body);
-    if (obj.materialsList) {
-        if (obj.materialsList.length === 1) {
-            let train_12306 = $prefs.valueForKey("train_12306");
-            if (train_12306) {
-                console.log(obj.materialsList[0].title);
-                let arr = train_12306.split(",");
-                obj.materialsList[0].filePath = undefined;
-                obj.materialsList[0].billId = arr[1];
-                obj.materialsList[0].billMaterialsId = arr[2];
-                obj.advertParam.skipTime = 1;
-            } else {
-                if (obj.materialsList[0].title === "铁路e卡通" || obj.materialsList[0].title === "12306订餐") {
-                    let timestamp = new Date().getTime();
-                    let train_12306 = timestamp + "," + obj.materialsList[0].billId + "," + obj.materialsList[0].billMaterialsId;
-                    let success = $prefs.setValueForKey(train_12306, "train_12306");
-                    if (success) {
-                        $notify("12306去广告", "", "获取广告参数成功，退后台重进即可干掉开屏广告");
-                    }
-                } else {
-                    let title = obj.materialsList[0].title;
-                    $notify("12306去广告", "", `获取广告参数失败，当前广告为${title}，请重复退后台重进的过程直到获取参数成功`);
-                }
-            }
-        } else if (obj.materialsList.length > 1) {
-            obj.materialsList = [{}];
-        }
-    }
-    $done({body: JSON.stringify(obj)});
-}
+// function removeAds() {
+//     var obj = JSON.parse($response.body);
+//     if (obj.materialsList) {
+//         if (obj.materialsList.length === 1) {
+//             let train_12306 = $prefs.valueForKey("train_12306");
+//             if (train_12306) {
+//                 console.log(obj.materialsList[0].title);
+//                 let arr = train_12306.split(",");
+//                 obj.materialsList[0].filePath = undefined;
+//                 obj.materialsList[0].billId = arr[1];
+//                 obj.materialsList[0].billMaterialsId = arr[2];
+//                 obj.advertParam.skipTime = 1;
+//             } else {
+//                 if (obj.materialsList[0].title === "铁路e卡通" || obj.materialsList[0].title === "12306订餐") {
+//                     let timestamp = new Date().getTime();
+//                     let train_12306 = timestamp + "," + obj.materialsList[0].billId + "," + obj.materialsList[0].billMaterialsId;
+//                     let success = $prefs.setValueForKey(train_12306, "train_12306");
+//                     if (success) {
+//                         $notify("12306去广告", "", "获取广告参数成功，退后台重进即可干掉开屏广告");
+//                     }
+//                 } else {
+//                     let title = obj.materialsList[0].title;
+//                     $notify("12306去广告", "", `获取广告参数失败，当前广告为${title}，请重复退后台重进的过程直到获取参数成功`);
+//                 }
+//             }
+//         } else if (obj.materialsList.length > 1) {
+//             obj.materialsList = [{}];
+//         }
+//     }
+//     $done({body: JSON.stringify(obj)});
+// }
 
 function removeValue() {
     let success = $prefs.removeValueForKey("train_12306");
