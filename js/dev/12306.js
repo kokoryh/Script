b1ce57a99c5df93e2b3c4c7a4a39ba16435c2238
kokoryh <kokoryh@qq.com>
2022-12-29 13:34:26 +0800

    添加时间戳以备后续更新需要

--- js/dev/12306.js
+++ js/dev/12306.js
@@ -13,12 +13,13 @@ function removeAds() {
                 console.log(obj.materialsList[0].title);
                 let arr = train_12306.split(",");
                 obj.materialsList[0].filePath = undefined;
-                obj.materialsList[0].billId = arr[0];
-                obj.materialsList[0].billMaterialsId = arr[1];
+                obj.materialsList[0].billId = arr[1];
+                obj.materialsList[0].billMaterialsId = arr[2];
                 obj.advertParam.skipTime = 1;
             } else {
                 if (obj.materialsList[0].title === "铁路e卡通" || obj.materialsList[0].title === "12306订餐") {
-                    let train_12306 = obj.materialsList[0].billId + "," + obj.materialsList[0].billMaterialsId;
+                    let timestamp = new Date().getTime();
+                    let train_12306 = timestamp + "," + obj.materialsList[0].billId + "," + obj.materialsList[0].billMaterialsId;
                     let success = $prefs.setValueForKey(train_12306, "train_12306");
                     if (success) {
                         $notify("12306去广告", "", "获取广告参数成功，退后台重进即可干掉开屏广告");
