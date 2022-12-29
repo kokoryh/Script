20177f4ec4d39f5aab147985b96527d5b293ea1c
kokoryh <kokoryh@qq.com>
2022-12-29 09:31:35 +0800

    Update 12306.js

--- js/dev/12306.js
+++ js/dev/12306.js
@@ -1,7 +1,10 @@
 var obj = JSON.parse($response.body);
 if (obj.materialsList) {
     if (obj.materialsList.length === 1) {
+        console.log(obj.materialsList[0].title);
         obj.materialsList[0].filePath = undefined;
+        obj.materialsList[0].billId = "4769";
+        obj.materialsList[0].billMaterialsId = "6390";
         obj.advertParam.skipTime = 1;
     } else if (obj.materialsList.length > 1) {
         obj.materialsList = [{}];
