bae94686904d7d253e19ab48f338caae4fce4c8a
kokoryh <kokoryh@qq.com>
2023-01-03 15:26:45 +0800

    Update amap.js

--- js/dev/amap.js
+++ js/dev/amap.js
@@ -3,8 +3,8 @@ if (obj.msgs && obj.msgs.length) {
     for (const item of obj.msgs) {
         item.body = '啊哈哈哈哈';
         item.showBody = '啊哈哈哈哈';
-        item.starttime = 1988121600;
-        item.expiretime = 1988207999;
+        // item.starttime = 1988121600;
+        // item.expiretime = 1988207999;
     }
     // obj.msgs = [];
     $done({body: JSON.stringify(obj)});
