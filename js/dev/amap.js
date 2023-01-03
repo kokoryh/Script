d2bcc320b46e6f03b29fb3374f6e14902bc16b63
kokoryh <kokoryh@qq.com>
2023-01-03 15:13:19 +0800

    Update amap.js

--- js/dev/amap.js
+++ js/dev/amap.js
@@ -1,11 +1,12 @@
 let obj = JSON.parse($response.body);
 if (obj.msgs && obj.msgs.length) {
-    for (const item of obj.msgs) {
-        item.body = '';
-        item.showBody = undefined;
-        item.starttime = 1988121600;
-        item.expiretime = 1988207999;
-    }
+    // for (const item of obj.msgs) {
+    //     item.body = '';
+    //     item.showBody = undefined;
+    //     item.starttime = 1988121600;
+    //     item.expiretime = 1988207999;
+    // }
+    obj.msgs = [];
     $done({body: JSON.stringify(obj)});
 } else {
     $done();
