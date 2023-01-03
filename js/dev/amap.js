20ef154ecc0b79a8b2fea4051dbb186c4bde471b
kokoryh <kokoryh@qq.com>
2023-01-03 14:50:14 +0800

    Update amap.js

--- js/dev/amap.js
+++ js/dev/amap.js
@@ -2,6 +2,8 @@ let obj = JSON.parse($response.body);
 if (obj.msgs && obj.msgs.length) {
     for (const item of obj.msgs) {
         item.body = '';
+        item.starttime = 1988121600;
+        item.expiretime = 1988207999;
     }
     $done({body: JSON.stringify(obj)});
 } else {
