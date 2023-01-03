a7b97672b7cf4ab28ca89279cc359a40726674c6
kokoryh <kokoryh@qq.com>
2023-01-03 15:37:33 +0800

    Update amap.js

--- js/dev/amap.js
+++ js/dev/amap.js
@@ -1,14 +1,12 @@
 let obj = JSON.parse($response.body);
 if (obj.msgs && obj.msgs.length) {
     for (const item of obj.msgs) {
-        item.body = '啊哈哈哈哈';
-        item.showBody = '啊哈哈哈哈';
+        item.body = '1';
+        item.showBody = '1';
         // item.starttime = 1988121600;
         // item.expiretime = 1988207999;
     }
-    // obj.msgs = [];
     $done({body: JSON.stringify(obj)});
 } else {
     $done();
 }
-// 查看2022打车记忆，领新年礼
