2a894334557d70e877fe4fa86fc53b2aec18a897
kokoryh <kokoryh@qq.com>
2023-01-04 17:47:43 +0800

    update amap，thx ddgksf2013

--- js/dev/amap.js
+++ js/dev/amap.js
@@ -1,7 +1,43 @@
-let obj = JSON.parse($response.body);
-if (obj.msgs) {
-    obj.msgs = undefined;
-    $done({body: JSON.stringify(obj)});
+let url = $request.url
+let obj = JSON.parse($response.body)
+let change = false
+if (url.includes("valueadded/alimama/splash_screen")) {
+    if (obj.data?.ad) {
+        for (const item of obj.data.ad) {
+            item.set.setting.display_time = 0
+            item.creative[0].start_time = 2240150400
+            item.creative[0].end_time = 2240150400
+        }
+        change = true
+    }
+} else if (url.includes("faas/amap-navigation/main-page")) {
+    if (obj.data?.cardList) {
+        obj.data.cardList = Object.values(obj.data.cardList.filter(item => {
+            return item.dataType === "LoginCard"
+        }))
+        change = true
+    }
+} else if (url.includes("dsp/profile/index/nodefaas")) {
+    obj.data.tipData = undefined
+    if (obj.data?.cardList) {
+        obj.data.cardList = Object.values(obj.data.cardList.filter(item => {
+            return item.dataType === "MyOrderCard" || item.dataType === "GdRecommendCard"
+        }))
+        change = true
+    }
+} else if (url.includes("search/new_hotword")) {
+    if (obj.data?.header_hotword) {
+        obj.data.header_hotword = []
+        change = true
+    }
+} else if (url.includes("ws/msgbox/pull")) {
+    if (obj.msgs) {
+        obj.msgs = undefined
+        change = true
+    }
+}
+if (change) {
+    $done({body: JSON.stringify(obj)})
 } else {
-    $done();
+    $done()
 }
