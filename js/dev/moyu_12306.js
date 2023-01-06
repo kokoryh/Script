de604ad21650b642aaef76eb5203b497c1887ed1
kokoryh <kokoryh@qq.com>
2023-01-06 17:45:40 +0800

    update

--- js/dev/moyu_12306.js
+++ js/dev/moyu_12306.js
@@ -5,7 +5,7 @@ if (obj.materialsList) {
     if (obj.materialsList.length === 1) {
         obj.materialsList[0].filePath = "https://api.dujin.org/bing/m.php"
         obj.materialsList[0].billId = undefined
-        obj.materialsList[0].billMaterialsId = "1000000"
+        obj.materialsList[0].billMaterialsId = "1000"
         obj.advertParam.skipTime = 1
     } else if (obj.materialsList.length > 1) {
         obj.materialsList = [{}]
