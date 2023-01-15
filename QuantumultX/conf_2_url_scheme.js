f26cf4487fbb13a2c6394e4afa4b4da80717c590
kokoryh <kokoryh@qq.com>
2023-01-15 09:32:46 +0800

    update resource

--- QuantumultX/conf_2_url_scheme.js
+++ QuantumultX/conf_2_url_scheme.js
@@ -1,13 +1,14 @@
 const fs = require('fs')
 const config = {
-    inputFile: ['resource']
+    inputFile: ['resource'],
+    header: 'https://quantumult.app/x/open-app/add-resource?remote-resource='
 }
 for (const filename of config.inputFile) {
     const content = fs.readFileSync(`./${filename}.conf`, 'UTF-8')
     const lines = content.split(/\r?\n/)
     let result = handleLines(lines)
-    writeFile(`./${filename}.md`, result.result)
-    writeFile(`./${filename}.txt`, result.simple_result)
+    writeFile(`./${filename}.md`, result.result_md)
+    writeFile(`./${filename}.txt`, result.result_txt)
 }
 
 function writeFile(outputPath, content) {
@@ -19,35 +20,35 @@ function writeFile(outputPath, content) {
 }
 
 function handleLines(lines) {
-    let result = ''
-    let simple_result = ''
-    let tagStack = []
+    let result_md = ''
+    let result_txt = ''
+    let myStack = []
     lines.forEach(line => {
         let l = line.trim()
-        if (l) {
+        if (l) { // 非空行的情况
             if (l.startsWith('#')) {
-                if (/#.*收集/.test(l)) {
-                    result += '# QX去广告和功能性资源收集(一键安装需qx版本≥1.0.30)\n'
-                    simple_result += 'QX去广告和功能性资源收集\n'
-                } else if (/#.*(分流|重写)$/.test(l)) {
+                if (/#.*收集/.test(l)) {  // 标题
+                    result_md += '# QuantumultX 资源收集(一键安装需qx版本≥1.0.30)\n'
+                    result_txt += 'QuantumultX 资源收集\n'
+                } else if (/#\s(功能性|去广告)(分流|重写)$/.test(l)) {
                     let content = l.split('#')[1].trim()
-                    result += `## ${content}\n`
-                    simple_result += `\n${content}\n`
-                } else if (l.startsWith('# @')) {
-                    let tmp = l.split(/\s+/)
-                    let author = tmp[1]
-                    let repo = tmp[2]
-                    result += `## [${author}](${repo})\n`
-                    simple_result += `\n${author}\n`
-                } else if (l.startsWith('# *')) {
-                    let tmp = l.split(/#\s\*/)
-                    result += `#### ${tmp[1]}\n`
-                    simple_result += `\n${tmp[1]}\n`
+                    result_md += `## ${content}\n`
+                    result_txt += `${content}\n`
+                } else if (l.startsWith('# @')) {  // 作者
+                    let content = l.split(/\s+/)
+                    let author = content[1]
+                    let link = content[2]
+                    result_md += `### [${author}](${link})\n`
+                    result_txt += `${author}\n`
+                } else if (l.startsWith('# *')) {  // 注释
+                    let content = l.split(/#\s\*/)[1]
+                    result_md += `#### ${content}\n`
+                    result_txt += `${content}\n`
                 } else {
-                    let tmp = l.split('#')
-                    let tmp2 = tmp[1].split(/[,，(（]/)
-                    tagStack.push(tmp2[0].trim())
-                    if (tmp2.length > 1) tagStack.push(l.match(/[^,，(（]*(.+)/)[1].trim())
+                    let tmp = l.split(/#\s*/)[1]
+                    let content = tmp.split(/[\s,，(（]/)
+                    myStack.push(content[0].trim())
+                    if (content.length > 1) myStack.push(l.match(/[^,，(（]*(.+)/)[1].trim())
                 }
             } else if (l.startsWith('[') || l.startsWith(';')) {
 
@@ -58,19 +59,25 @@ function handleLines(lines) {
                 } else {
                     url_encoded_json['rewrite_remote'] = [l]
                 }
-                let eURL = 'https://quantumult.app/x/open-app/add-resource?remote-resource=' + encodeURIComponent(JSON.stringify(url_encoded_json))
-                if (tagStack.length > 1) {
-                    let part1 = tagStack.shift()
-                    let part2 = tagStack.shift()
-                    result += `#### [${part1}](${eURL}) ${part2}\n`
-                    simple_result += `${part1}${part2}\n${eURL}\n`
+                let eURL = config.header + encodeURIComponent(JSON.stringify(url_encoded_json))
+                if (!myStack.length) {
+                    let tag = l.match(/tag=([^,]*)/)[1]
+                    myStack.push(tag)
+                }
+                if (myStack.length > 1) {
+                    let part1 = myStack.shift()
+                    let part2 = myStack.shift()
+                    result_md += `[${part1}](${eURL}) ${part2}  \n`
+                    result_txt += `${part1}${part2}\n${eURL}\n`
                 } else {
-                    let part1 = tagStack.shift()
-                    result += `#### [${part1}](${eURL})\n`
-                    simple_result += `${part1}\n${eURL}\n`
+                    let part1 = myStack.shift()
+                    result_md += `[${part1}](${eURL})  \n`
+                    result_txt += `${part1}\n${eURL}\n`
                 }
             }
+        } else { // 空行的情况
+            result_txt += '\n'
         }
     })
-    return {result, simple_result}
+    return {result_md, result_txt}
 }
