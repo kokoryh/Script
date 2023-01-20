5fc3b80d153ea6e0ed7fa2130496cd9b18b95102
kokoryh <kokoryh@qq.com>
2023-01-20 15:09:05 +0800

    update ip

--- js/dev/netease.js
+++ js/dev/netease.js
@@ -1,10 +1,10 @@
 let obj = JSON.parse($response.body)
 if (obj.data?.["interface3.music.163.com"]) {
     obj.data["interface3.music.163.com"].ip = obj.data["interface3.music.163.com"].ip.filter(item => {
-        return item.startsWith("39.105.175")  // 112.13这段ip不给MITM
-            || item.startsWith("39.105.63")
-            || item.startsWith("59.111.19")
-            || item.startsWith("59.111.160")
+        return item.startsWith("59.111")  // 112.13这段ip不给MITM
+        // || item.startsWith("39.105.63")
+        // || item.startsWith("59.111.19")
+        // || item.startsWith("59.111.160")
     })
     if (obj.data["interface3.music.163.com"].ip.length) {
         $done({body: JSON.stringify(obj)})
