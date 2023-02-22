7f45b390dcedfe2cc4c7cf735c36b25847ac8f37
kokoryh <kokoryh@qq.com>
2023-02-22 09:28:00 +0800

    update 12306

--- js/dev/12306.js
+++ js/dev/12306.js
@@ -26,6 +26,17 @@
 removeAds();
 
 function removeAds() {
+    let body;
+    let obj = JSON.parse($request.body);
+    if (obj.placementNo === "0007") {
+        body = '{materialsList:[{billMaterialsId:"255",filePath:"h",creativeType:1}],advertParam:{skipTime:1}}'
+    } else {
+        body = '{code:"00",message:"无广告返回"}'
+    }
+    $done({body})
+}
+
+function removeAdsOld() {
     let obj = JSON.parse($response.body);
     if (obj.materialsList) {
         if (obj.materialsList.length === 1) {
