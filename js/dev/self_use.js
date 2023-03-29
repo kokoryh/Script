00a18d1944b824cc7b511c64112e75dc8ea47547
kokoryh <kokoryh@qq.com>
2023-03-29 16:38:31 +0800

    test

--- js/dev/self_use.js
+++ js/dev/self_use.js
@@ -27,7 +27,7 @@ if (url.includes("manga.bilibili.com")) {  // 哔哩漫画
     let header = $request.headers
     let ua = header["User-Agent"] || header["user-agent"]
     if (ua.includes("AMap") || ua.includes("Cainiao")) {
-        if ('undefined' !== typeof $task) $done({statusCode: 'HTTP/1.1 404 Not Found'})
+        if ('undefined' !== typeof $task) $done({status: 'HTTP/1.1 404 Not Found'})
         else $done()
     }
     else $done({})
