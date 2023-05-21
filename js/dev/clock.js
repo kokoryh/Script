3e42e746a63aebe1c51fdf8424f75da000855021
kokoryh <kokoryh@qq.com>
2023-05-21 16:44:42 +0800

    update

---
+++ js/dev/clock.js
@@ -0,0 +1,34 @@
+const $ = kokoryh();
+const date = new Date();
+if (date.getDay() !== 6) {
+    $.notify('打卡');
+} else {
+    if (new Date(date.getTime() + 7 * 86400 * 1000).getMonth() !== date.getMonth()) {
+        $.notify('打卡');
+    }
+}
+$done();
+
+function kokoryh() {
+    const isQuanX = 'undefined' !== typeof $task;
+    const isSurge = 'undefined' !== typeof $httpClient;
+    const notify = (title, subtitle, message) => {
+        if (isQuanX) $notify(title, subtitle, message);
+        if (isSurge) $notification.post(title, subtitle, message);
+    }
+    const getValue = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key);
+        if (isSurge) return $persistentStore.read(key);
+    }
+    const setValue = (val, key) => {
+        if (isQuanX) return $prefs.setValueForKey(val, key);
+        if (isSurge) return $persistentStore.write(val, key);
+    }
+    return {
+        isQuanX,
+        isSurge,
+        notify,
+        getValue,
+        setValue
+    }
+}
