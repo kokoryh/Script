036c4b9998ce770e1fd91b875bddf6922da2cbf0
kokoryh <kokoryh@qq.com>
2024-12-09 20:52:29 +0800

    新版模块测试

--- js/dev/bilibili.json.js
+++ js/dev/bilibili.json.js
@@ -52,16 +52,16 @@ function handleLayout(body) {
         {
             pos: 4,
             id: 545,
-            name: "追番",
+            name: "动画",
             tab_id: "bangumi",
             uri: "bilibili://pgc/home",
         },
         {
             pos: 5,
-            id: 774,
-            name: "动画",
-            tab_id: "anime",
-            uri: "bilibili://following/home_activity_tab/6544",
+            id: 151,
+            name: "影视",
+            tab_id: "film",
+            uri: "bilibili://pgc/cinema-tab",
         },
     ];
     body.data.top = [
@@ -74,19 +74,35 @@ function handleLayout(body) {
             icon: "http://i0.hdslb.com/bfs/archive/d43047538e72c9ed8fd8e4e34415fbe3a4f632cb.png",
         },
     ];
-    if (body.data.bottom?.length > 3) {
-        body.data.tab[4] = {
+    body.data.bottom = [
+        {
+            pos: 1,
+            id: 177,
+            name: "首页",
+            tab_id: "home",
+            uri: "bilibili://main/home/",
+            icon: "http://i0.hdslb.com/bfs/archive/63d7ee88d471786c1af45af86e8cb7f607edf91b.png",
+            icon_selected: "http://i0.hdslb.com/bfs/archive/e5106aa688dc729e7f0eafcbb80317feb54a43bd.png",
+        },
+        {
+            pos: 2,
+            id: 179,
+            name: "动态",
+            tab_id: "dynamic",
+            uri: "bilibili://following/home/",
+            icon: "http://i0.hdslb.com/bfs/archive/86dfbe5fa32f11a8588b9ae0fccb77d3c27cedf6.png",
+            icon_selected: "http://i0.hdslb.com/bfs/archive/25b658e1f6b6da57eecba328556101dbdcb4b53f.png",
+        },
+        {
             pos: 5,
-            id: 151,
-            name: "影视",
-            tab_id: "film",
-            uri: "bilibili://pgc/cinema-tab",
-        };
-        const bottomList = ["首页", "动态", "我的"];
-        body.data.bottom = body.data.bottom.filter((e) =>
-            bottomList.includes(e.name)
-        );
-    }
+            id: 181,
+            name: "我的",
+            tab_id: "我的Bottom",
+            uri: "bilibili://user_center/",
+            icon: "http://i0.hdslb.com/bfs/archive/4b0b2c49ffeb4f0c2e6a4cceebeef0aab1c53fe1.png",
+            icon_selected: "http://i0.hdslb.com/bfs/archive/a54a8009116cb896e64ef14dcf50e5cade401e00.png",
+        },
+    ];
     $done({ body: JSON.stringify(body) });
 }
 
@@ -96,7 +112,7 @@ function handleSplash(body) {
         if (body.data[key]) {
             body.data[key] = [];
         }
-    })
+    });
     $done({ body: JSON.stringify(body) });
 }
 
